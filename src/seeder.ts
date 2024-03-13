8bdd94031977abdc6743470de722ea7dd86dd3ea
loekung <lnslove5893@gmail.com>
2024-03-14 00:57:58 +0700

    feat: seeder user mockup

--- src/seeder.ts
+++ src/seeder.ts
@@ -1,20 +1,16 @@
 import { PrismaClient } from '@prisma/client';
 import { readFileSync } from 'fs';
 import { CommandRunner, Command } from 'nest-commander';
+import { UserService } from './user/user.service';
 @Command({
   name: 'seed',
 })
 export class CommandSeeder extends CommandRunner {
-  constructor() {
+  constructor(private readonly userService: UserService) {
     super();
   }
 
-  async run(
-    passedParams: string[],
-    options?: Record<string, any>,
-  ): Promise<void> {
-    console.log('CLI Params', passedParams);
-    console.log('CLI Options', options);
+  async run(): Promise<void> {
     await this.loadData()
     return Promise.resolve(undefined);
   }
@@ -22,42 +18,32 @@ export class CommandSeeder extends CommandRunner {
   async loadData() {
     const prisma = new PrismaClient()
     try {
-      // await prisma.users.deleteMany()
-      // console.log('Deleted records in users table')
+
       await prisma.$connect()
+
+      await prisma.users.deleteMany()
+      console.log('Deleted records in users table')
 
       await prisma.posts.deleteMany()
       console.log('Deleted records in posts table')
 
-      const rawData = readFileSync('./posts.json', { encoding: 'utf8' })
-      const dataJson = JSON.parse(rawData)
-      dataJson.map((item) => {
+      const rawUser = readFileSync('./users.json', { encoding: 'utf8' })
+      const usersJson = JSON.parse(rawUser)
+      await this.userService.createMany(usersJson)
+      console.log('Seeded users data')
+
+      const rawPosts = readFileSync('./posts.json', { encoding: 'utf8' })
+      const postsJson = JSON.parse(rawPosts)
+      postsJson.map((item) => {
         item.postedAt = new Date(item.postedAt)
       })
 
       await prisma.posts.createMany({
-        data: dataJson
+        data: postsJson
       })
-      console.log('Added posts data')
+      console.log('Seeded posts data')
 
 
-
-      // await prisma.$queryRaw`ALTER SEQUENCE Users_id_seq RESTART WITH 0`
-      // console.log('reset users auto increment to 1')
-
-      // await prisma.$queryRaw`ALTER TABLE public."Posts" AUTO_INCREMENT = 1`
-      // console.log('reset posts auto increment to 1')
-
-
-      // await prisma.users.createMany({
-      //   data: categories
-      // })
-      // console.log('Added user data')
-
-
-      // await prisma.posts.createMany({
-      //   data: products
-      // })
 
     } catch (e) {
       console.error(e)
