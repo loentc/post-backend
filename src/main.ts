a1eda53820e762640ba668e43ea5a9b04a721c9e
loekung <lnslove5893@gmail.com>
2024-03-18 19:17:28 +0700

    feat: set helmet

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 const port = process.env.PORT || 8100;
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: true });
     app.useGlobalPipes(new ValidationPipe({ transform: true }));
+    app.use(helmet());
     await app.listen(port);
     Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
